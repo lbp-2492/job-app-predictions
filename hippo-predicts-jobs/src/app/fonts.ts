949602b971cd1b60d1eb0209cdcfdc38f312f872
lpinn <lep86@pitt.edu>
2024-08-05 15:14:33 -0400

    got font to work

---
+++ hippo-predicts-jobs/src/app/fonts.ts
@@ -0,0 +1,6 @@
+import { Changa } from 'next/font/google'
+
+export const changa = Changa({
+  subsets: ['latin'],
+  display: 'swap',
+})
--- hippo-predicts-jobs/src/app/page.tsx
+++ hippo-predicts-jobs/src/app/page.tsx
@@ -1,26 +1,38 @@
 import Link from 'next/link';
+import Head from 'next/head';
+
 
 export default function Home() {
   return (
-    <main className="landing-page-bg">
-      <div>
-        <h1 className="">HIPPO'S PREDICTION</h1>
-      </div>
-
-      <div className="cont">
-        <div className="grid-container">
-          <Link className="link-wrapper" href="">
-            <div className="grid-item">analytics</div>
-          </Link>
-          <Link className="link-wrapper" href="/input">
-            <div className="grid-item">data input</div>
-          </Link>
-          <Link className="link-wrapper" href="">
-            <div className="grid-item">raw data</div>
-          </Link>
+    <>
+      <Head>
+        <style>
+          @import url('https://fonts.googleapis.com/css2?family=Changa:wght@200;300;400;600&family=Poppins:ital,wght@0,100;0,300;0,500;0,600;0,700;0,800;0,900;1,300&family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+        </style>
+      </Head>
+    
+      
+      <main className="landing-page-bg">
+        <div>
+          <h1 className="">HIPPO'S PREDICTION</h1>
         </div>
-      </div>
-    </main>
+
+        <div className="cont">
+          <div className="grid-container">
+            <Link className="link-wrapper" href="">
+              <div className="grid-item">analytics</div>
+            </Link>
+            <Link className="link-wrapper" href="/input">
+              <div className="grid-item">data input</div>
+            </Link>
+            <Link className="link-wrapper" href="">
+              <div className="grid-item">raw data</div>
+            </Link>
+          </div>
+        </div>
+      </main>
+    </>
+    
   );
 }
 
